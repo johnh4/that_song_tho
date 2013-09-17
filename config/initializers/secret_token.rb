df70fb869603ff0f98374cca14b780f1d936803c
johnh4 <jehowl4@gmail.com>
2013-09-17 16:33:01 -0400

    Initial commit. Created CSS for landing page using foundation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThatSongTho::Application.config.secret_key_base = 'ed6cd9883619231120f199ac7290f026b3a35b92d543bfdb1ede08650d77d7c4a28fe8ab715089d53979ec87eb3de2706c44e6ffbab410ae14fc9cf64531960a'
